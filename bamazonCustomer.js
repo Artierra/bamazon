31e2e0b2bd077dd51db1d12af0e99f1b65afce03
Monica Bongue <muddyforkfarm@gmail.com>
2019-10-26 16:15:50 -0400

    initial files

---
+++ bamazonCustomer.js
@@ -0,0 +1,9 @@
+//first prompt user for a product id
+//second prompt - how many they want to buy
+//after customer checks order-check the store if there is enough
+//if not log -insufficent quantity and prevent the order from going through
+//if enough- fullfill customers order-update SQL database to show remaining quantity
+//show customer total price of purchase
+// require inquirer and sql code
+//check connection
+//make query to database
