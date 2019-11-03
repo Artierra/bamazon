3ee9b85cc03ce793057e2cff798b6caa85336b48
Monica Bongue <muddyforkfarm@gmail.com>
2019-11-03 12:39:51 -0500

    accessing individual records

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -48,34 +48,102 @@ function ProductInfo() {
 
 var askforInput = function () {
     inquirer
-        .prompt([{
-                name: "productID",
-                type: "input",
-                message: "What is the ID of the product you would like to buy?",
-                validate: function (value) {
-                    if (isNaN(value) === false) {
-                        return true;
-                    }
-                    return false;
+        .prompt({
+            name: "productID",
+            type: "input",
+            message: "What is the ID of the product you would like to buy?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
                 }
-            },
-            {
-                name: "stockQuantity",
-                type: "input",
-                message: "How many lbs would like to buy?",
-                validate: function (value) {
-                    if (isNaN(value) === false) {
-                        return true;
-                    }
-                    return false;
-                }
+                return false;
             }
-        ])
+        })
+        .then(function (userInput1) {
+            var selection = userInput1.productID;
+            connection.query("SELECT * FROM products WHERE item_id = ?", selection, function (err, res) {
+                if (err) {
+                    throw err;
+                    console.log(res);
+                } else if (res.length === 0) {
+                    console.log("That product does not exist, please enter a product id from the list above");
+                    askforInput();
 
+                } else {
+                    inquirer
+                        .prompt({
+                            name: "stockQuantity",
+                            type: "input",
+                            message: "How many lbs would like to buy?",
+                            validate: function (value) {
+                                if (isNaN(value) === false) {
+                                    return true;
+                                }
+                                return false;
+                            }
+                        })
+                        .then(function (userInput2) {
+                            var stockQuantity = userInput2.stockQuantity;
+                            if (stockQuantity > res[0].stock_quantity) {
+                                console.log("Sorry we only have " + res[0].stock_quantity + " lbs left")
+                                askforInput();
+                            } else {
 
+                            }
+                        })
+                }
 
-        .then(function (userinput) {
-            console.log(userinput.productID);
-            console.log(userinput.stockQuantity);
+            });
         });
 }
+
+
+
+
+
+//     {
+//         name: "stockQuantity",
+//         type: "input",
+//         message: "How many lbs would like to buy?",
+//         validate: function (value) {
+//             if (isNaN(value) === false) {
+//                 return true;
+//             }
+//             return false;
+//         }
+//     }
+// ])
+
+// .then(function (userinput1) {
+//     var selection = userinput1.productID;
+//     connection.query("SELECT * FROM products WHERE item_id=?"), selection,
+//         function (err, res) {
+//             if (err) throw err;
+//             if (res.length === 0) {
+//                 console.log("That product does not exist, please enter a product id from the lsit above")
+//                 askforInput()
+//             } else {
+//                 console.log('OK, we have that');
+//             }
+//         }
+//     console.log(userinput.productID);
+
+
+//console.log(userinput.stockQuantity);
+
+
+//enough update database to remove # of  Item
+//console log updated quantit from database
+//console.log(# of items * price)
+//} else {
+//prevent order from going throu
+//console.log(Insufficient Quantity)
+// }
+
+
+// Once the customer has placed the order, your application should check
+// if your store has enough of the product to meet the customer 's request.
+// If not, the app should log a phrase like`Insufficient quantity!`, and then prevent the order
+// from going through. 8. However,if your store _does_ have enough of the product, you should fulfill
+// the customer 's order. this means updating the SQL database to reflect the remaining quantity.
+// Once the update goes through, show the customer the total cost of their pur
